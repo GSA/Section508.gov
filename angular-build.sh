d1494a1ea1eaa6a675b3a4263003cae9acbda1e9
Alex <alex.ntowe@gsa.gov>
2023-05-17 15:11:18 -0400

    Update angular-build.sh

--- angular-build.sh
+++ angular-build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # angular build script for ART
 
-url='https://federalist-7a132a2e-6307-4cd0-9f82-e30e871d214a.sites.pages.cloud.gov/preview/gsa/section508.gov/'; 
-echo 'Demo url -->' $url ${BRANCH}; 
-[ ${BRANCH} = 'main' ] && echo $url${BRANCH};  
-[ ${BRANCH} != 'main' ] && echo $url${BRANCH};
+baseurl='https://federalist-7a132a2e-6307-4cd0-9f82-e30e871d214a.sites.pages.cloud.gov/preview/gsa/section508.gov/'; 
+url=$url${BRANCH}
+echo 'Demo url -->' $url; 
+[ ${BRANCH} = 'main' ] && echo 'Section508';  
+[ ${BRANCH} != 'main' ] && echo $url;
