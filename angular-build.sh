d6e50ff578cb04d8fc059d892d147509b5ac3421
Alex <alex.ntowe@gsa.gov>
2023-05-17 15:39:59 -0400

    Developing the build script

--- angular-build.sh
+++ angular-build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# Basic if statement
-if [ $1 -gt 100 ]
-then
-echo CUrrent Branch: ${BRANCH}.
-pwd
-fi
+# angular build script for ART
+#npm link @angular/cli;
+baseurl='https://federalist-7a132a2e-6307-4cd0-9f82-e30e871d214a.sites.pages.cloud.gov/preview/gsa/section508.gov/'; 
+url=$baseurl${BRANCH}"/art/"
+echo 'Demo url -->' $url; 
+[ ${BRANCH} = 'main' ] && ng build --configuration production --base-href=https://www.section508.gov/art/;  
+[ ${BRANCH} != 'main' ] && ng build --configuration dev --base-href=$url;
