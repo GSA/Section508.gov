3bc0fe5c4530e72716e5ab9bd1efeb2f4a4e95ce
Alex <alex.ntowe@gsa.gov>
2023-05-17 15:05:10 -0400

    Update angular-build.sh

--- angular-build.sh
+++ angular-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Basic if statement
-if [ 12 -gt 10 ]
-then
-echo Current Branch: ${BRANCH}.
-pwd
-fi
+# angular build script for ART
+
+url = 'https://federalist-7a132a2e-6307-4cd0-9f82-e30e871d214a.sites.pages.cloud.gov/preview/gsa/section508.gov/'; 
+echo 'Demo url -->' $url${BRANCH}; 
+[ ${BRANCH} = 'main' ] && echo $url${BRANCH};  
+[ ${BRANCH} != 'main' ] && echo $url${BRANCH};
