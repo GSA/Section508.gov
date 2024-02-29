03d69af4403e972dab0f2c78a341f7a6f3ace09f
Illia Stankevych <illia.stankevych@gsa.gov>
2024-02-28 21:58:42 -0500

    ECASOGP-11018: Change header, page url settings

--- assets/js/external-508-blocks.js
+++ assets/js/external-508-blocks.js
@@ -3,14 +3,14 @@ sidenav: false
 ---
 {% raw %}
 document.addEventListener('DOMContentLoaded', function() {
- let header = `{% endraw %}{% include header.html %}{% include menu.html primary_navigation=site.primary_navigation  secondary_navigation=site.secondary_navigation %}{% raw %}`;
+ let header = `{% endraw %}{% include header_banner.html %}{% include menu.html primary_navigation=site.primary_navigation  secondary_navigation=site.secondary_navigation %}{% raw %}`;
  const footer = `{% endraw %}{% include footer.html %}{% raw %}`;
  header += '<div id="main-content"></div>';
  document.getElementById('header-508').innerHTML = header
-   .replace(/src="\//g, 'src="https://www.section508.gov/')
-   .replace(/href="\//g, 'href="https://www.section508.gov/');
+   .replace(/src="\//g, 'src="{% endraw %}{{ site.url }}{% raw %}/')
+   .replace(/href="\//g, 'href="{% endraw %}{{ site.url }}{% raw %}/');
  document.getElementById('footer-508').innerHTML = footer
-   .replace(/src="\//g, 'src="https://www.section508.gov/')
-   .replace(/href="\//g, 'href="https://www.section508.gov/');
+   .replace(/src="\//g, 'src="{% endraw %}{{ site.url }}{% raw %}/')
+   .replace(/href="\//g, 'href="{% endraw %}{{ site.url }}{% raw %}/');
 });
 {% endraw %}
