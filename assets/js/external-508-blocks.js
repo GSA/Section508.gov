51f895209531f86e4926378d3d0f28a038ae10b1
Illia Stankevych <illia.stankevych@gsa.gov>
2024-02-29 10:03:44 -0500

    ECASOGP-11018: Change external blocks styles

--- assets/js/external-508-blocks.js
+++ assets/js/external-508-blocks.js
@@ -11,7 +11,6 @@ sidenav: false
 document.addEventListener('DOMContentLoaded', function() {
  let header = `{% endraw %}{% include header_banner.html %}{% include menu.html primary_navigation=site.primary_navigation  secondary_navigation=site.secondary_navigation %}{% raw %}`;
  const footer = `{% endraw %}{% include footer.html %}{% raw %}`;
- header += '<div id="main-content"></div>';
  document.getElementById('header-508').innerHTML = header
    .replace(/src="\//g, 'src="{% endraw %}{{ myUrlPrefix }}{% raw %}/')
    .replace(/href="\//g, 'href="{% endraw %}{{ myUrlPrefix }}{% raw %}/');
