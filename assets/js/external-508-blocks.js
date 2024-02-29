3caead913fa501281870760a2f6b9c115bd9f35f
Illia Stankevych <illia.stankevych@gsa.gov>
2024-02-28 21:09:18 -0500

    ECASOGP-11018: Add styles for external blocks (footer and header)

--- assets/js/external-508-blocks.js
+++ assets/js/external-508-blocks.js
@@ -3,9 +3,14 @@ sidenav: false
 ---
 {% raw %}
 document.addEventListener('DOMContentLoaded', function() {
- const header = `{% endraw %}{% include header.html %}{% include menu.html primary_navigation=site.primary_navigation  secondary_navigation=site.secondary_navigation %}{% raw %}`;
+ let header = `{% endraw %}{% include header.html %}{% include menu.html primary_navigation=site.primary_navigation  secondary_navigation=site.secondary_navigation %}{% raw %}`;
  const footer = `{% endraw %}{% include footer.html %}{% raw %}`;
- document.getElementById('header-508').innerHTML = header;
- document.getElementById('footer-508').innerHTML = footer;
+ header += '<div id="main-content"></div>';
+ document.getElementById('header-508').innerHTML = header
+   .replace(/src="\//g, 'src="https://www.section508.gov/')
+   .replace(/href="\//g, 'href="https://www.section508.gov/');
+ document.getElementById('footer-508').innerHTML = footer
+   .replace(/src="\//g, 'src="https://www.section508.gov/')
+   .replace(/href="\//g, 'href="https://www.section508.gov/');
 });
 {% endraw %}
