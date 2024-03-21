4d3f71b7d56bac610138522f64222b6d1445c75f
Harika Munipalle <harika.munipalle@gsa.gov>
2024-03-20 23:07:49 -0400

    alert

--- assets/js/external-508-blocks.js
+++ assets/js/external-508-blocks.js
@@ -18,5 +18,9 @@ document.addEventListener('DOMContentLoaded', function () {
         .replace(/src="\//g, 'src="{% endraw %}{{ myUrlPrefix }}{% raw %}/')
         .replace(/href="\//g, 'href="{% endraw %}{{ myUrlPrefix }}{% raw %}/');
 });
-
+document.addEventListener('click', function () {
+    if (window.location.href.indexOf("openacr") >= 0) {
+        return 'Navigating to this link will navigate away from the ACR Editor. If you continue, you will lose the data you have entered so far. Click "Cancel" to remain in the ACR Editor. Click "OK" to proceed and lose any data already entered.';
+    }
+});
 {% endraw %}
