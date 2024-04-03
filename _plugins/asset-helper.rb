051007ce497f180133e3a9097358142f47c6e8d9
Luna Wang <lunawang@lunas-mbp.lan>
2024-04-02 20:28:23 -0400

    installed uswds 3.7

--- _plugins/asset-helper.rb
+++ _plugins/asset-helper.rb
@@ -3,9 +3,9 @@ require 'fileutils'
 module AssetHelper
     def self.process(site, payload)
         return if @processed
-        FileUtils.cp_r('node_modules/uswds/dist/js/', 'assets/', verbose: true)
-        FileUtils.cp_r('node_modules/uswds/dist/img/', 'assets/', verbose: true)
-        FileUtils.cp_r('node_modules/uswds/dist/fonts/', 'assets/', verbose: true)
+        FileUtils.cp_r('node_modules/@uswds/uswds/dist/js/', 'assets/', verbose: true)
+        FileUtils.cp_r('node_modules/@uswds/uswds/dist/img/', 'assets/', verbose: true)
+        FileUtils.cp_r('node_modules/@uswds/uswds/dist/fonts/', 'assets/', verbose: true)
         @processed = true
     end
 end
