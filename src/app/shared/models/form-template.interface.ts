5e4d0691dc535e6ea9ad93843f17948786b3add5
yogita <yogita@decisionscienceanalytics.com>
2024-10-25 09:54:29 -0400

    Revert "support section label changed to title - reverting"
    This reverts commit 1d993da6f93bf9933879e5e305b77e498bd86dcd.

--- src/app/shared/models/form-template.interface.ts
+++ src/app/shared/models/form-template.interface.ts
@@ -7,26 +7,31 @@ export interface FormTemplateInterface{
      * @description: The data need to generate a control (input text, checkbox, radio button)
      */
     formElements:FormElement[];
+
     /**
      * @type string
      * @description: The navigation buttons for the form | submit, next, back, add
      */
     formButtons: FormButtons;
+
     /**
      * @type string
      * @description: Rather or not the form should be cleared after submission
      */
     clearForm: boolean;
+
     /**
      * @type string
      * @description: If the form should be disabled or not 
      */
     disable?: boolean;
+
     /**
      * @description to identify each formControl
      * @type number
      */
     id?:number;
+
     /**
      * @type string
      * @description: Used to stored the data from the form
