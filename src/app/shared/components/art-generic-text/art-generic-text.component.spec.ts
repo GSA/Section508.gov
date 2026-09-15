4cde2aab8a3d21942a05e8ca2b3ed98b7cae8de0
Andrew Nielson <29068131+drewnielson@users.noreply.github.com>
2026-09-15 15:13:11 -0400

    Update ART landing page to include SRT (#1487)
    * Add SRT card to ART intro
    
    * change sizing
    
    * Fix spacing; revert banner and title; combine cards
    
    * Update header/intro content
    
    * update review/updated date
    
    * Update note text
    
    * Split intro text and warning note; add note syling
    
    * Update intro text and standards link
    
    * Add external link class
    
    * Open SRT in new tab
    
    * Open SRT in new tab - take 2
    
    * add .gov and .edu users qualification
    
    * Fix console logging and ART video sizing style

--- src/app/shared/components/art-generic-text/art-generic-text.component.spec.ts
+++ src/app/shared/components/art-generic-text/art-generic-text.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ArtGenericTextComponent } from './art-generic-text.component';
 
@@ -6,7 +6,7 @@ describe('ArtGenericTextComponent', () => {
   let component: ArtGenericTextComponent;
   let fixture: ComponentFixture<ArtGenericTextComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ArtGenericTextComponent ]
     })
